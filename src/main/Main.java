be6412d6a3267b71cc192ceb09e11d61dc76dd12
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2024-11-23 13:33:05 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         // Testando criação de categoria
         try {
-            Categoria categoria = new Categoria("Eletrônicos", "Produtos eletrônicos diversos");
+            Categoria categoria = new Categoria("Alimentos melhores", "Produtos de alimentos melhores diversos");
             categoriaDAO.cadastrarCategoria(categoria);
             System.out.println("Categoria criada com sucesso!");
         } catch (SQLException e) {
@@ -37,16 +37,16 @@ public class Main {
 
         // Testando criação de produto
         try {
-            // Primeiro, precisamos definir um ID de categoria existente no banco de dados
-            int idCategoria = 1; // Supondo que a categoria tenha o ID 1 no banco de dados
+            //definir um ID de categoria existente no banco de dados
+            int idCategoria = 5; // Supondo que a categoria tenha o ID 1 no banco de dados
             Categoria categoria = new Categoria(idCategoria);
 
             Produto produto = new Produto(
-                "Smartphone",
-                "Smartphone de última geração",
+                "Sucrilhos",
+                "Sucrilhos Kellogs",
                 10,
-                1200.00,
-                1500.00,
+                10.00,
+                15.00,
                 categoria
             );
 
@@ -54,6 +54,52 @@ public class Main {
             System.out.println("Produto criado com sucesso!");
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+        
+        // testando alteração de produto
+        try {
+        	int idCategoria = 5;
+        	Categoria categoria = new Categoria(idCategoria);
+        	
+        	Produto produtoAtualizado = new Produto(
+        			"sucrilhos",
+        			"Sucrilhos kellogs melhor",
+        			15,
+        			12.00,
+        			18.00,
+        			categoria
+        	);
+        	produtoAtualizado.setId(4);
+        	
+        	produtoDAO.atualizarProduto(produtoAtualizado);
+        } catch (SQLException e){
+        	e.printStackTrace();
+        }
+        
+        // Testando exclusão de produtos
+        try {
+        	int idProduto = 4;
+        	produtoDAO.excluirProduto(idProduto);
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+        
+        // testando exclusão de categoria
+        try {
+        	int idCategoria = 5;
+        	categoriaDAO.excluirCategoria(idCategoria);
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+        
+        // testando edição de categoria
+        try {
+        	Categoria categoriaAtualizada = new Categoria("Comidinhas", "Comidinhas boas!");
+        	categoriaAtualizada.setId(4);
+        	
+        	categoriaDAO.editarCategoria(categoriaAtualizada);
+        } catch (SQLException e) {
+        	e.printStackTrace();
         }
     }
 }
