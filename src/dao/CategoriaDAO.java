46b334c99aa0581b83885fa940a010894badef71
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2024-11-23 13:35:05 -0300

    Criando métodos de crud para categoria

--- src/dao/CategoriaDAO.java
+++ src/dao/CategoriaDAO.java
@@ -35,4 +35,43 @@ public class CategoriaDAO {
 		}
 		return categorias;
 	}
+	
+	public void excluirCategoria(int idCAtegoria) throws SQLException {
+		String sqlProdutos = "DELETE FROM produtos WHERE id_categoria = ?";
+		String sqlCategoria = "DELETE FROM categorias WHERE id_categoria = ?";
+		try (Connection conn = DatabaseConnection.getConnection()) {
+			// excluindo produtos
+			try (PreparedStatement stmtProdutos = conn.prepareStatement(sqlProdutos)) {
+				stmtProdutos.setInt(1, idCAtegoria);
+				stmtProdutos.executeUpdate();
+			}
+			
+			// excluindo a categoria
+			try (PreparedStatement stmtCategoria = conn.prepareStatement(sqlCategoria)) {
+				stmtCategoria.setInt(1, idCAtegoria);
+				int rowsAffected = stmtCategoria.executeUpdate();
+				if (rowsAffected > 0) {
+					System.out.println("Categoria excluida com sucesso");
+				} else {
+					System.out.println("Categoria não encontrada");
+				}
+			}
+		}
+	}
+	
+	public void editarCategoria (Categoria categoria) throws SQLException {
+		String sql = "UPDATE categorias SET nome_categoria = ?, descricao_categoria = ? WHERE id_categoria = ?";
+		try (Connection conn = DatabaseConnection.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, categoria.getNome());
+			stmt.setString(2, categoria.getDescricao());
+			stmt.setInt(3, categoria.getId());
+			int rowsAffected = stmt.executeUpdate();
+			if (rowsAffected > 0) {
+				System.out.println("Categoria editada com sucesso");
+			} else {
+				System.out.println("Categoria não encontrada");
+			}
+		}
+	}
 }
