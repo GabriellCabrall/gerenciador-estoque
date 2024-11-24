06c9347168e081102c1bf1b3087c19789cdd4638
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2024-11-24 15:56:02 -0300

    Atualizar o DatabaseConnection.java

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/cadastroprodutos";
     private static final String USER = "root";
-    private static final String PASSWORD = "11092001Cabral";
+    private static final String PASSWORD = "*************";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
